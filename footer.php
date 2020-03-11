77946cf404ffc600e315217f60e6af2efc0df89a
convoymedia-paul <paul@convoymedia.com>
2020-03-11 14:26:19 +0000

    footer styling

--- footer.php
+++ footer.php
@@ -10,16 +10,8 @@
  * @since Starkers 3.0
  */
 ?>
-
-<?php
-	/* A sidebar in the footer? Yep. You can can customize
-	 * your footer with four columns of widgets.
-	 */
-	get_sidebar( 'footer' );
-?>
-
-			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+<div id="footer">
+</div>
 
 <?php
 	/* Always have wp_footer() just before the closing </body>
