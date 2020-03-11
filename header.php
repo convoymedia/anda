67b14a85b4ab559d895bbf34d27cb801cd9e7cbe
convoymedia-paul <paul@convoymedia.com>
2020-03-11 12:27:34 +0000

    header not syncing?

--- header.php
+++ header.php
@@ -54,8 +54,4 @@
 	<div id="access" role="navigation">
 		test
 		<?php echo get_header_image(); ?>
-	  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-		<a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a>
-		<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
 	</div><!-- #access -->
