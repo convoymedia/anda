e74377a5b733025dd8a22ba3ddd451aaa6a8dbb1
convoymedia-paul <paul@convoymedia.com>
2020-03-11 12:38:43 +0000

    adding menu

--- header.php
+++ header.php
@@ -51,6 +51,9 @@
 		</div>
 	</div>
 
-	<div id="access" role="navigation">
-		<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><img src="<?php echo get_header_image(); ?>" /></a>
+	<div id="access" class="container" role="navigation">
+		<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><img src="<?php echo get_header_image(); ?>" />
+		
+		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+	</a>
 	</div><!-- #access -->
