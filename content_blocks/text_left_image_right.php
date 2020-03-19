67feec1f03e09db8bd8edb264e658fa805b41a5f
convoymedia-paul <paul@convoymedia.com>
2020-03-19 13:21:59 +0000

    image 100

--- content_blocks/text_left_image_right.php
+++ content_blocks/text_left_image_right.php
@@ -1,3 +1,3 @@
 <div class="text_left_image_right">
-    <img src="<?php echo get_template_directory_uri(); ?>/shades_grey_curved.png" />
+    <img src="<?php echo get_template_directory_uri(); ?>/images/shades_grey_curved.png" style="width:100%" />
 </div>
