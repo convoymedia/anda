30db358bb456485b167aafca462c5310660fc8b9
convoymedia-paul <paul@convoymedia.com>
2020-03-19 12:20:36 +0000

    typo fixes

--- content_blocks.php
+++ content_blocks.php
@@ -2,7 +2,7 @@
     if (have_rows('block')) {
         while (have_rows('block')) {
             the_row();
-            include "content_blocks/" . get_row_layout() . ".php"
+            include "content_blocks/" . get_row_layout() . ".php";
         }
     }
 ?>
