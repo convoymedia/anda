c1dc467d719331c6e4ffa8b4c3e3c715848de7c9
convoymedia-paul <paul@convoymedia.com>
2020-03-19 12:24:59 +0000

    ifwhile

--- content_blocks.php
+++ content_blocks.php
@@ -1,6 +1,8 @@
 <?php
     if (have_rows('block')) {
+        echo "if";
         while (have_rows('block')) {
+            echo "while";
             the_row();
             echo "content_blocks/" . get_row_layout() . ".php";
             include "content_blocks/" . get_row_layout() . ".php";
