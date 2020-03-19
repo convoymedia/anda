9787a7733120386805f97a84eb83c664f3a06ba7
convoymedia-paul <paul@convoymedia.com>
2020-03-19 13:30:07 +0000

    other image

--- content_blocks/text_left_image_right.php
+++ content_blocks/text_left_image_right.php
@@ -1,3 +1,4 @@
 <div class="text_left_image_right">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/shades_grey_curved.png" style="width:100%" />
+    <img src="<?php echo get_template_directory_uri(); ?>/images/shades_grey_curved.png" style="position:absolute;top:0px;left:0px;width:100%;z-index:2" />
+    <img src="<?php the_sub_field("image"); ?>" />
 </div>
