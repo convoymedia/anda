5a9095e350e07315346dc25d546ff3ac71811ed2
convoymedia-paul <paul@convoymedia.com>
2020-03-19 15:02:32 +0000

    services

--- content_blocks/two_column_image_text.php
+++ content_blocks/two_column_image_text.php
@@ -1,12 +1,17 @@
-<div>
+<div class="two_column_image_text">
     <h2><?php the_sub_field('title'); ?></h2>
-    <img src="<?php the_sub_field("left_image"); ?>" />
-    <?php the_sub_field("left_sub_title"); ?>
-    <?php the_sub_field("left_text"); ?>
-    <a href="<?php the_sub_field("left_button_link"); ?>"><button><?php the_sub_field("left_button_title"); ?></button></a>
-
-    <img src="<?php the_sub_field("right_image"); ?>" />
-    <?php the_sub_field("right_sub_title"); ?>
-    <?php the_sub_field("right_text"); ?>
-    <a href="<?php the_sub_field("right_button_link"); ?>"><button><?php the_sub_field("right_button_title"); ?></button></a>
+    <div class="column-holder">
+        <div>
+            <img src="<?php the_sub_field("left_image"); ?>" />
+            <h3><?php the_sub_field("left_sub_title"); ?></h3>
+            <?php the_sub_field("left_text"); ?>
+            <a href="<?php the_sub_field("left_button_link"); ?>"><button><?php the_sub_field("left_button_title"); ?></button></a>
+        </div>
+        <div>
+                <img src="<?php the_sub_field("right_image"); ?>" />
+                <h3><?php the_sub_field("right_sub_title"); ?></h3>
+                <?php the_sub_field("right_text"); ?>
+                <a href="<?php the_sub_field("right_button_link"); ?>"><button><?php the_sub_field("right_button_title"); ?></button></a>
+            </div>
+        </div>
 </div>
