df1339c34e2abbce0c2338151447a09dcf33d4f9
convoymedia-paul <paul@convoymedia.com>
2020-03-19 13:56:08 +0000

    page

--- content_blocks/text_left_image_right.php
+++ content_blocks/text_left_image_right.php
@@ -2,7 +2,7 @@
     <img src="<?php echo get_template_directory_uri(); ?>/images/shades_grey_curved.png" class="main-image" />
     <img src="<?php the_sub_field("image"); ?>" class="underlay-image" />
     <div class="overlay">
-        <?php the_sub_field("title"); ?>
-        <?php the_sub_field("content"); ?>co
+        <h2><?php the_sub_field("title"); ?></h2>
+        <?php the_sub_field("content"); ?>
     </div>
 </div>
