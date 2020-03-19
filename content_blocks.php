e6680685549d0ce8caea2c39f0c1b905cbf1978c
convoymedia-paul <paul@convoymedia.com>
2020-03-19 12:23:34 +0000

    echo

--- content_blocks.php
+++ content_blocks.php
@@ -2,6 +2,7 @@
     if (have_rows('block')) {
         while (have_rows('block')) {
             the_row();
+            echo "content_blocks/" . get_row_layout() . ".php";
             include "content_blocks/" . get_row_layout() . ".php";
         }
     }
