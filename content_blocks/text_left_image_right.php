887a5b75283ce7f1ecf21626f262d0f6af998b04
convoymedia-paul <paul@convoymedia.com>
2020-03-20 15:37:26 +0000

    back to array

--- content_blocks/text_left_image_right.php
+++ content_blocks/text_left_image_right.php
@@ -1,6 +1,6 @@
 <div class="text_left_image_right">
     <img src="<?php echo get_template_directory_uri(); ?>/images/shades_grey_curved.png" class="main-image" />
-    <?php echo wp_get_attachment_image_src(get_sub_field("image"), "shades_grey_curved"); ?>
+    <?php print_r(wp_get_attachment_image_src(get_sub_field("image"), "shades_grey_curved")); ?>
     <img src="<?php the_sub_field("image"); ?>" class="underlay-image" />
     <div class="overlay">
         <h2><?php the_sub_field("title"); ?></h2>
