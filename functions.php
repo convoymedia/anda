3eab747ad493040500f3674fe3c24fc12fafded3
convoymedia-paul <paul@convoymedia.com>
2020-04-21 23:53:31 +0100

    form fixes

--- functions.php
+++ functions.php
@@ -550,7 +550,7 @@ add_shortcode('quote', 'convoyQuote');
 
 function my_search_form( $form ) {
     $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
-	<div style="border-bottom:1px solid black;">
+	<div>
 	<button type="submit" id="searchsubmit"><i class="far fa-search"></i></button>
     <input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="Search" />
     </div>
