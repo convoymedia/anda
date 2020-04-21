4dac448884703e544ae3985477f8b1168d7a924e
convoymedia-paul <paul@convoymedia.com>
2020-04-21 15:54:55 +0100

    why no show blog filter

--- functions.php
+++ functions.php
@@ -402,7 +402,7 @@ function twentyten_widgets_init() {
 	register_sidebar( array(
 		'name' => __( 'Blog Filter', 'twentyten' ),
 		'id' => 'blog-filter-widget-area',
-		'description' => __( 'Widget area for top bar', 'twentyten' ),
+		'description' => __( 'Widget area for blog filter', 'twentyten' ),
 		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 		'after_widget' => '</li>',
 		'before_title' => '<h3 class="widget-title">',
