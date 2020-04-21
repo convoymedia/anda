7f900e542a2f6dd8396ffff3fdef7eb25699fd0f
convoymedia-paul <paul@convoymedia.com>
2020-04-21 13:15:42 +0100

    curved top

--- content_blocks/text_left_image_right.php
+++ content_blocks/text_left_image_right.php
@@ -78,6 +78,7 @@
             case "dark_grey_curved":
                 $image = wp_get_attachment_image_src(get_sub_field("image"), "dark_grey_curved"); 
                 ?>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/curned-top.png" style="width:100%;height:auto;" />
                 <img src="<?php echo get_template_directory_uri(); ?>/images/dark-blob2.png" style="background-image:url(<?php echo $image[0]; ?>);" />
                 <div class="overlay">
                     <h2><?php the_sub_field("title"); ?></h2>
