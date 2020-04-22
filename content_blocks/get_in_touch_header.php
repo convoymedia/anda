f751122691684cd6d6ea5458d08e87a39d00ff55
convoymedia-paul <paul@convoymedia.com>
2020-04-22 15:49:55 +0100

    blobby

--- content_blocks/get_in_touch_header.php
+++ content_blocks/get_in_touch_header.php
@@ -1,6 +1,6 @@
 <div class="blob_holder get_in_touch_header">
     <img src="<?php the_sub_field("bg_image"); ?>" />
-    <div>
+    <div class="blobby">
         <img src="<?php the_sub_field("blob_image"); ?>" />
         <div><?php the_sub_field("title"); ?></div>
     </div>
