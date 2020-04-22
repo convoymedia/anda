daf9d3fd99e3021b52226f068cb6219d5fd253bf
convoymedia-paul <paul@convoymedia.com>
2020-04-22 16:18:02 +0100

    get in touch tet

--- content_blocks/get_in_touch.php
+++ content_blocks/get_in_touch.php
@@ -1,6 +1,9 @@
 <div class="get_in_touch" style="background-color:<?php the_sub_field("background_colour"); ?>">
     <div class="touch-holder">
         <img src="<?php the_sub_field("image"); ?>" />
+        <div class="the-text-holder">
+            <?php the_sub_field("left_text"); ?>
+        </div>
         <div class="form-holder">
             <h2><?php the_sub_field("title"); ?></h2>
             <?php echo do_shortcode('[contact-form-7 id="167"]'); ?>
