72734cddbc6b5b5651380dc00ef62b98c6f544a9
convoymedia-paul <paul@convoymedia.com>
2020-04-22 14:32:40 +0100

    get in touch

--- header.php
+++ header.php
@@ -64,5 +64,6 @@
 	<div id="access" class="container smaller" role="navigation">
 		<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo alignleft"><img src="<?php echo get_header_image(); ?>" /></a>
 		<?php wp_nav_menu( array( 'container_class' => 'alignright', 'theme_location' => 'primary' ) ); ?>
+		<div class="get-in-touch">Get in touch</div>
 		<br class="clear" />
 	</div><!-- #access -->
