d293a3053c41128e630308e5088d2f97c3d58919
convoymedia-paul <paul@convoymedia.com>
2020-04-22 15:20:08 +0100

    blob not blog

--- content_blocks/get_in_touch_header.php
+++ content_blocks/get_in_touch_header.php
@@ -1,7 +1,7 @@
 <div class="blob_holder get_in_touch_header">
     <img src="<?php the_sub_field("bg_image"); ?>" />
     <div>
-        <img src="<?php the_sub_field("blog_image"); ?>" />
+        <img src="<?php the_sub_field("blob_image"); ?>" />
         <?php the_sub_field("title"); ?>
     </div>
 </div>
