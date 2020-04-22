f6dd09e72c2702e9e5ce77ad35bf5c651750eba3
convoymedia-paul <paul@convoymedia.com>
2020-04-22 11:55:04 +0100

    break youtube

--- single.php
+++ single.php
@@ -56,7 +56,7 @@ get_header(); ?>
                     switch(get_field("type")) {
                         case "video":
                             $v = get_field("video_details");
-							echo $v['video_url'];
+							?></div><div class="iframe-grow"><?php echo $v['video_url']; ?></div><div class="post-content"><?php
                         break;
                     }
                 ?>
