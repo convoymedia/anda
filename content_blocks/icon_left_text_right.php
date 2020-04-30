902ede59b803a2977713c31c883c24b86c30c9bd
convoymedia-paul <paul@convoymedia.com>
2020-04-30 11:25:30 +0100

    div holder

--- content_blocks/icon_left_text_right.php
+++ content_blocks/icon_left_text_right.php
@@ -1,4 +1,5 @@
 <div class="icon_left_text_right">
+    <div>
     <div class="empty"><!-- empty here for testing --></div>
     <div class="first">
         <h2>
@@ -6,5 +7,6 @@
             <div><img src="<?php the_sub_field("image"); ?>" /></div>
         </h2>
         <div><?php the_sub_field("text");?></div>
+    </div>
     </div>
 </div>
